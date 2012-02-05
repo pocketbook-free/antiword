52752e96b85aee87120be6d357fd6d90d61079a4
pocketbook-free <andrey.kalyaka@pocketbook-global.com>
2012-02-05 18:05:38 +0200

    adding antiword repo

---
+++ CMakeFiles/antiword.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/kalyaka/pocketbook_free/antiword/src/asc85enc.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/asc85enc.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/blocklist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/blocklist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/chartrans.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/chartrans.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/datalist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/datalist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/depot.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/depot.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/dib2eps.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/dib2eps.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/doclist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/doclist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/fail.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/fail.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/finddata.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/finddata.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/findtext.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/findtext.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/fmt_text.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/fmt_text.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/fontlist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/fontlist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/fonts.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/fonts.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/fonts_u.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/fonts_u.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/hdrftrlist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/hdrftrlist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/imgexam.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/imgexam.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/imgtrans.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/imgtrans.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/jpeg2eps.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/jpeg2eps.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/listlist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/listlist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/main_u.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/main_u.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/misc.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/misc.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/notes.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/notes.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/options.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/options.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/out2window.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/out2window.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/output.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/output.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/pdf.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/pdf.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/pictlist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/pictlist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/png2eps.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/png2eps.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/postscript.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/postscript.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/prop0.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/prop0.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/prop2.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/prop2.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/prop6.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/prop6.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/prop8.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/prop8.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/properties.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/properties.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/propmod.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/propmod.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/rowlist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/rowlist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/sectlist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/sectlist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/stylelist.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/stylelist.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/stylesheet.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/stylesheet.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/summary.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/summary.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/tabstop.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/tabstop.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/text.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/text.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/unix.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/unix.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/utf8.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/utf8.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/word2text.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/word2text.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/worddos.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/worddos.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/wordlib.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/wordlib.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/wordmac.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/wordmac.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/wordole.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/wordole.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/wordwin.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/wordwin.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/xmalloc.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/xmalloc.c.o"
+  "/home/kalyaka/pocketbook_free/antiword/src/xml.c" "/home/kalyaka/pocketbook_free/antiword/CMakeFiles/antiword.dir/src/xml.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "PLATFORM_FC"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
